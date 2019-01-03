ce6b4887b08a9fcbae7e353a7ed7a9605bbb55cf
qindongliang <qindongliang@xueleyun.com>
2019-01-03 14:30:42 +0800

    启动脚本修改支持scala普通项目

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -2,12 +2,17 @@
 
 shell_path=$(cd `dirname $0`; pwd)
 project_path=`dirname $shell_path`
-
-echo "脚本路径：$shell_path"
+scala_path="$SCALA_HOME/lib/*"
+echo "脚径：$shell_path"
 echo "项目根路径: $project_path"
 
 
-jars="${project_path}/libs/*"
+jars="$project_path/libs/"
 
-cpath="${jars}:${project_path}/conf:"
+echo "jar的："$jars
+
+cpath="$jars:$project_path/conf/:$project_path/easy-package-template-1.0.0.jar:$scala_path"
+
+echo $cpath
+
 java -cp $cpath start.App
