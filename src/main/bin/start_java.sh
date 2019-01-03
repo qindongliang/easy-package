727bfe14ea5f62a104c82b1d34adc3ef3993855d
qindongliang <qindongliang@xueleyun.com>
2019-01-03 19:04:53 +0800

    脚本修改，增加java的脚本

--- src/main/bin/start_java.sh
+++ src/main/bin/start_java.sh
@@ -5,17 +5,16 @@
 shell_path=$(cd `dirname $0`; pwd)
 #项目根路径
 project_path=`dirname $shell_path`
-#scala的依赖，单独纯scala项目需要
-scala_path="$SCALA_HOME/lib/*"
+
 echo "脚本路径：$shell_path"
 echo "项目根路径: $project_path"
+
 #依赖的jar
 jars="$project_path/libs/*"
 
-echo "jar的："$jars
 
-cpath="$jars:$project_path/conf/*:$project_path/easy-package-template-1.0.0.jar:$scala_path"
+cpath="$jars:$project_path/conf/*:$project_path/easy-package-template-1.0.0.jar:"
 
 echo $cpath
 
-java -cp $cpath start.App
+java -cp $cpath demo.Test
