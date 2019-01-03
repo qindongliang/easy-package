1f934b951b9cae1434c1cec8993ecc2043330926
qindongliang <qindongliang@xueleyun.com>
2019-01-03 17:49:04 +0800

    添加测试内容

--- src/main/scala/start/App.scala
+++ src/main/scala/start/App.scala
@@ -2,22 +2,25 @@ package start
 
 import java.util
 
+import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.serializer.SerializeFilter
-import com.alibaba.fastjson.{JSON, JSONObject}
+import com.typesafe.config.ConfigFactory
+import org.apache.log4j.Logger
 
 object App {
 
-
+  val conf=ConfigFactory.load("conf.properties")
+   val logger = Logger.getLogger(classOf[App])
   def main(args: Array[String]): Unit = {
 
 
-    println("ok , it is beautiful.")
+    logger.info(s" appId: ${conf.getString("appId")} ")
+    logger.warn(" this is a warn message.")
 
     val list=new util.ArrayList[String]()
     list.add("string1")
     list.add("string2")
     list.add("string3")
-    println(list.toString)
     println(JSON.toJSONString(list,new Array[SerializeFilter](0)))
 
   }
